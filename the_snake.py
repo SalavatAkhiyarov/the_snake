f11eb88ed50bcb214c69af5127c90ef480972e24
Salavat Akhiyarov <salavatahiyarov@icloud.com>
2025-04-11 17:29:27 +0300

    2 спринт(7)

--- the_snake.py
+++ the_snake.py
@@ -67,7 +67,7 @@ class Apple(GameObject):
         while True:
             new_x = (randint(0, GRID_WIDTH - 1)) * GRID_SIZE
             new_y = (randint(0, GRID_HEIGHT - 1)) * GRID_SIZE
-            if (new_x,new_y) not in use_positions:
+            if (new_x, new_y) not in use_positions:
                 return new_x, new_y
 
     def draw(self):
